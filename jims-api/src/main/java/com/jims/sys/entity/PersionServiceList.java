baa690c25b3575dfae2a5516165d0915da1c9090
yangruidong <18745200375@126.com>
2016-04-18 11:26:49 +0800

    Dao和实体类

---
+++ jims-api/src/main/java/com/jims/sys/entity/PersionServiceList.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.jims.sys.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jims.common.persistence.DataEntity;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Date;
+
+/**
+ * 人员拥有服务Entity
+ * @author yangruidong
+ * @version 2016-04-13
+ */
+public class PersionServiceList extends DataEntity<PersionServiceList> {
+	
+	private static final long serialVersionUID = 1L;
+	private String persionId;		// 人员
+	private String menuId;		// 菜单
+	private String flag;		// 0默认服务1，增值服务
+	private Date serviceStartDate;		// 服务开始时间
+	private Date serviceEndDate;		// 服务结束时间
+	
+	public PersionServiceList() {
+		super();
+	}
+
+	public PersionServiceList(String id){
+		super(id);
+	}
+
+	@Length(min=0, max=64, message="人员长度必须介于 0 和 64 之间")
+	public String getPersionId() {
+		return persionId;
+	}
+
+	public void setPersionId(String persionId) {
+		this.persionId = persionId;
+	}
+	
+	@Length(min=0, max=64, message="菜单长度必须介于 0 和 64 之间")
+	public String getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+	
+	@Length(min=0, max=2, message="0默认服务1，增值服务长度必须介于 0 和 2 之间")
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	public Date getServiceStartDate() {
+		return serviceStartDate;
+	}
+
+	public void setServiceStartDate(Date serviceStartDate) {
+		this.serviceStartDate = serviceStartDate;
+	}
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	public Date getServiceEndDate() {
+		return serviceEndDate;
+	}
+
+	public void setServiceEndDate(Date serviceEndDate) {
+		this.serviceEndDate = serviceEndDate;
+	}
+	
+}
