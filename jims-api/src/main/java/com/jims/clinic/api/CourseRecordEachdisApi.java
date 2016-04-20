f8be3529d3bf8acdf697b739017f664783653bd0
qinlongxin <443971972@qq.com>
2016-04-20 18:02:19 +0800

    修改病程记录

---
+++ jims-api/src/main/java/com/jims/clinic/api/CourseRecordEachdisApi.java
@@ -0,0 +1,27 @@
+package com.jims.clinic.api;
+
+import com.jims.clinic.entity.CourseRecordEachdis;
+
+/**
+ * Created by qinlongxin on 2016/4/20.
+ */
+public interface CourseRecordEachdisApi {
+    /**
+     * 根据病程id查询每日病程记录信息
+     * @author qinlongxin
+     * @version 2016/4/20
+     */
+    public CourseRecordEachdis get(CourseRecordEachdis courseRecordEachdis);
+    /**
+     * 保存或编辑
+     * @author qinlongxin
+     * @version 2016/4/20
+     */
+    public void saveCourseRecordEachdis(CourseRecordEachdis courseRecordEachdis);
+    /**
+     * 通过病程记录主要表bingcheng_id查询每日病程
+     * @author qinlongxin
+     * @version 2016/4/20
+     */
+    public CourseRecordEachdis getEachdisByCourseRecordId(CourseRecordEachdis courseRecordEachdis);
+}
