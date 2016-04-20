5ad78adb4b83e29c50a8e078e8f22d8ea32f91e0
zhaoning12 <1174393128@qq.com>
2016-04-20 09:57:07 +0800

    病人主索引代码生成

---
+++ jims-api/src/main/java/com/jims/clinic/api/PatMasterIndexApi.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.jims.clinic.api;
+
+/**
+ * 病人主索引api
+ * @author zhaoning
+ * @version 2016-04-19
+ */
+public interface PatMasterIndexApi {
+
+}
