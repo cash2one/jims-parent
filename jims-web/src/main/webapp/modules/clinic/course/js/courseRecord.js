eff8f99d061839584213a127349497538df82b57
qinlongxin <443971972@qq.com>
2016-04-21 16:48:39 +0800

    修改本地病程记录

--- jims-web/src/main/webapp/modules/clinic/course/js/courseRecord.js
+++ jims-web/src/main/webapp/modules/clinic/course/js/courseRecord.js
@@ -9,7 +9,7 @@ function onloadMethod(){
         method:'get',
         collapsible:false,//是否可折叠的
         fit: true,//自动大小
-        url:basePath+'/dict/list',
+        url:basePath+'/courseRecord/list',
         remoteSort:false,
         idField:'fldId',
         singleSelect:false,//是否单选
@@ -18,11 +18,8 @@ function onloadMethod(){
         pageList: [10,15,30,50],//可以设置每页记录条数的列表
         rownumbers:true,//行号
         columns:[[      //每个列具体内容
-            {field:'value',title:'键值',width:'5%',align:'center'},
-            {field:'label',title:'标签',width:'18%',align:'center'},
+            {field:'luruShijian',title:'键值',width:'5%',align:'center'},
             {field:'type',title:'类型',width:'18%',align:'center'},
-            {field:'description',title:'描述',width:'20%',align:'center'},
-            {field:'sort',title:'排序',width:'5%',align:'center'},
             {field:'id',title:'操作',width:'30%',align:'center',formatter:function(value, row, index){
                 var html='<button class="easy-nbtn easy-nbtn-success easy-nbtn-s" onclick="look(\''+value+'\')"><img src="/static/images/index/icon1.png" width="12"/>查看</button>'+
                     '<button class="easy-nbtn easy-nbtn-info easy-nbtn-s" onclick="get(\''+value+'\')"><img src="/static/images/index/icon2.png"  width="12" />修改</button>'+
@@ -109,7 +106,7 @@ function deleteRow(id) {
 function del(id){
     $.ajax({
         'type': 'POST',
-        'url': basePath+'/dict/del',
+        'url': basePath+'/courseRecord/del',
         'contentType': 'application/json',
         'data': id=id,
         'dataType': 'json',
@@ -131,7 +128,7 @@ function del(id){
  * 保存方法
  */
 function saveDice(){
-    $.postForm(basePath+'/dict/save','dictForm',function(data){
+    $.postForm(basePath+'/courseRecord/save','courseRecordForm',function(data){
         if(data.data=='success'){
             $.messager.alert("提示消息",data.code+"条记录，保存成功");
             $("#dlg").dialog('close');
@@ -153,12 +150,12 @@ function get(id){
     $("#dlg").dialog({title: '修改字典信息'}).dialog("open");
     $.ajax({
         'type': 'post',
-        'url': basePath+'/dict/get',
+        'url': basePath+'/courseRecord/get',
         'contentType': 'application/json',
         'data': id=id,
         'dataType': 'json',
         'success': function(data){
-            $('#dictForm').form('load',data);
+            $('#courseRecordForm').form('load',data);
         }
     });
 }
@@ -171,12 +168,12 @@ function look(id){
     $("#saveBut").hide();
     $.ajax({
         'type': 'post',
-        'url': basePath+'/dict/get',
+        'url': basePath+'/courseRecord/get',
         'contentType': 'application/json',
         'data': id=id,
         'dataType': 'json',
         'success': function(data){
-            $('#dictForm').form('load',data);
+            $('#courseRecordForm').form('load',data);
         }
     });
 }
