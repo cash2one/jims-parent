bafdf2c52a5595507dc764e751cb9e51265249a4
qinlongxin <443971972@qq.com>
2016-04-21 10:44:51 +0800

    修改病程记录

---
+++ jims-service/src/main/java/com/jims/clinic/service/CourseRecordStageService.java
@@ -0,0 +1,13 @@
+package com.jims.clinic.service;
+
+
+import com.jims.clinic.dao.CourseRecordStageDao;
+import com.jims.clinic.entity.CourseRecordStage;
+import com.jims.common.service.impl.CrudImplService;
+
+/**
+ * Created by qinlongxin on 2016/4/21.
+ */
+public class CourseRecordStageService extends CrudImplService<CourseRecordStageDao,CourseRecordStage> {
+
+}
