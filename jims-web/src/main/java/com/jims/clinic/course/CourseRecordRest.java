fd6ed5f66f9a778b7a70e4565ed4fbc170e5a144
qinlongxin <443971972@qq.com>
2016-04-21 11:54:57 +0800

    修改病程记录

---
+++ jims-web/src/main/java/com/jims/clinic/course/CourseRecordRest.java
@@ -0,0 +1,43 @@
+package com.jims.clinic.course;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jims.clinic.api.CourseRecordApi;
+import com.jims.clinic.entity.CourseRecord;
+import com.jims.common.data.PageData;
+import com.jims.common.persistence.Page;
+import com.jims.sys.entity.Dict;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+
+/**
+ * Created by qinlongxin on 2016/4/21.
+ */
+@Component
+@Produces("application/json")
+@Path("courseRecord")
+public class CourseRecordRest {
+    @Reference(version = "1.0.0")
+    private CourseRecordApi courseRecordApi ;
+    /**
+     * 异步加载表格
+     * @param request
+     * @param response
+     * @return
+     */
+    @Path("list")
+    @GET
+    public PageData list(@Context HttpServletRequest request,@Context HttpServletResponse response){
+        Page<CourseRecord> page = courseRecordApi.findPage(new Page<CourseRecord>(request,response), new CourseRecord());
+        PageData pageData=new PageData();
+        pageData.setRows(page.getList());
+        pageData.setTotal(page.getCount());
+        return pageData;
+    }
+
+}
