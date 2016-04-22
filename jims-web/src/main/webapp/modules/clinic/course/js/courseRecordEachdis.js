a5fe4cf4a8dc6e45ab004241ab4f2865ff102ae0
qinlongxin <443971972@qq.com>
2016-04-22 15:02:29 +0800

    修改病程记录

---
+++ jims-web/src/main/webapp/modules/clinic/course/js/courseRecordEachdis.js
@@ -0,0 +1,17 @@
+/**
+ * Created by qinlongxin on 2016/4/22.
+ */
+function saveEachdis() {
+    formSubmitInput("eachdisForm");
+    $.postForm(basePath + "/courseRecordeachdis/save", "eachdisForm", function (data) {
+        if (data.code == "1") {
+            $.messager.alert("提示信息", "保存成功");
+        } else {
+            $.messager.alert("提示信息", "保存失败", "error");
+        }
+
+    }), function (data) {
+        $.messager.alert("提示信息", "保存失败", "error");
+    }
+}
+
