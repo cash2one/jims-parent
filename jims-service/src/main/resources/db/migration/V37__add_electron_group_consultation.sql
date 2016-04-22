820379069705f04805fff04013290fcf86c2282e
git1-xueyx <1264073776@qq.com>
2016-04-22 15:57:56 +0800

    会诊记录

---
+++ jims-service/src/main/resources/db/migration/V37__add_electron_group_consultation.sql
@@ -0,0 +1,54 @@
+﻿drop table ELECTRON_GROUP_CONSULTATION cascade constraints;
+
+/*==============================================================*/
+/* Table: ELECTRON_GROUP_CONSULTATION     会诊主表                      */
+/*==============================================================*/
+create table ELECTRON_GROUP_CONSULTATION 
+(
+   ID                   VARCHAR2(64)         not null,
+   ZHUYUAN_ID           VARCHAR2(64),
+   PATIENT_ID           VARCHAR2(64),
+   DOCTOR_ID            VARCHAR2(64),
+   GROUPTYPE            VARCHAR2(1),
+   SHENQINGSHIJIAN      TIMESTAMP,
+   BINGQINGZHAIYAO      CLOB,
+   HUIZHENLIYOU         CLOB,
+   HUIZHENYIJIAN        CLOB,
+   CREATE_DATE          TIMESTAMP,
+   UPDATE_DATE          TIMESTAMP,
+   CREATE_BY            VARCHAR2(255),
+   UPDATE_BY            VARCHAR2(255),
+   FABUFLAG             VARCHAR2(10),
+   DEL_FLAG             VARCHAR2(1),
+   constraint "PK_electron_group_consultation" primary key (ID)
+);
+
+comment on table ELECTRON_GROUP_CONSULTATION is
+'会诊记录';
+
+comment on column ELECTRON_GROUP_CONSULTATION.ZHUYUAN_ID is
+'住院号';
+
+comment on column ELECTRON_GROUP_CONSULTATION.PATIENT_ID is
+'患者id';
+
+comment on column ELECTRON_GROUP_CONSULTATION.DOCTOR_ID is
+'医生ID';
+
+comment on column ELECTRON_GROUP_CONSULTATION.GROUPTYPE is
+'类型：紧急、一般';
+
+comment on column ELECTRON_GROUP_CONSULTATION.SHENQINGSHIJIAN is
+'申请时间';
+
+comment on column ELECTRON_GROUP_CONSULTATION.BINGQINGZHAIYAO is
+'病情摘要';
+
+comment on column ELECTRON_GROUP_CONSULTATION.HUIZHENLIYOU is
+'会诊理由';
+
+comment on column ELECTRON_GROUP_CONSULTATION.HUIZHENYIJIAN is
+'会诊意见';
+
+comment on column ELECTRON_GROUP_CONSULTATION.FABUFLAG is
+'发布标志';
---
+++ jims-service/src/main/resources/db/migration/V38__add_electron_group_consultation_into.sql
@@ -0,0 +1,41 @@
+﻿drop table ELECTRON_GROUP_CONSULTATION_IN cascade constraints;
+
+/*==============================================================*/
+/* Table: ELECTRON_GROUP_CONSULTATION_IN      参与会诊表                  */
+/*==============================================================*/
+create table ELECTRON_GROUP_CONSULTATION_IN 
+(
+   ID                   VARCHAR2(64)         not null,
+   DOCTOR_ID            VARCHAR2(64),
+   HUIZHEN_ID           VARCHAR2(64),
+   HUIZHENYIJIAN        CLOB,
+   CREATE_DATE          TIMESTAMP,
+   UPDATE_DATE          TIMESTAMP,
+   CREATE_BY            VARCHAR2(255),
+   UPDATE_BY            VARCHAR2(255),
+   QIANMINGSTYPE        VARCHAR2(1),
+   QIANMINGSHIJIAN      TIMESTAMP,
+   QIANMING_ID          VARCHAR2(64),
+   constraint "PK_electron_group_consultation_into" primary key (ID)
+);
+
+comment on table ELECTRON_GROUP_CONSULTATION_IN is
+'参与会诊记录';
+
+comment on column ELECTRON_GROUP_CONSULTATION_IN.DOCTOR_ID is
+'医生ID';
+
+comment on column ELECTRON_GROUP_CONSULTATION_IN.HUIZHEN_ID is
+'参与会诊ID';
+
+comment on column ELECTRON_GROUP_CONSULTATION_IN.HUIZHENYIJIAN is
+'会诊意见';
+
+comment on column ELECTRON_GROUP_CONSULTATION_IN.QIANMINGSTYPE is
+'签名类型';
+
+comment on column ELECTRON_GROUP_CONSULTATION_IN.QIANMINGSHIJIAN is
+'签名时间';
+
+comment on column ELECTRON_GROUP_CONSULTATION_IN.QIANMING_ID is
+'医生签名';
