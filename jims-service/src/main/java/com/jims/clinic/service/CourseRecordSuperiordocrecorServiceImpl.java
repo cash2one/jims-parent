8f0824292a74118eff2fdcdf194923a22abfaf64
zhangpeng002 <2698869515@qq.com>
2016-04-23 08:41:38 +0800

    上级医生查房（api修改）

--- jims-service/src/main/java/com/jims/clinic/service/CourseRecordSuperiordocrecorServiceImpl.java
+++ jims-service/src/main/java/com/jims/clinic/service/CourseRecordSuperiordocrecorServiceImpl.java
@@ -4,7 +4,7 @@
 package com.jims.clinic.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
-import com.jims.clinic.api.CourseRecordSuperiordocrecordApi;
+import com.jims.clinic.api.CourseRecordSuperiorDocrecorApi;
 import com.jims.clinic.dao.CourseRecordDao;
 import com.jims.clinic.dao.CourseRecordSuperiordocrecorDao;
 import com.jims.clinic.entity.CourseRecord;
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service(version = "1.0.0")
 @Transactional(readOnly = true)
-public class CourseRecordSuperiordocrecorServiceImpl extends CrudImplService<CourseRecordSuperiordocrecorDao, CourseRecordSuperiordocrecor> implements CourseRecordSuperiordocrecordApi {
+public class CourseRecordSuperiordocrecorServiceImpl extends CrudImplService<CourseRecordSuperiordocrecorDao, CourseRecordSuperiordocrecor> implements CourseRecordSuperiorDocrecorApi {
 
     @Autowired
     private CourseRecordSuperiordocrecorDao courseRecordSuperiordocrecorDao;
--- jims-web/src/main/java/com/jims/clinic/CourseRecordSuperiordocrecorRest.java
+++ jims-web/src/main/java/com/jims/clinic/CourseRecordSuperiordocrecorRest.java
@@ -1,15 +1,12 @@
 package com.jims.clinic;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.jims.clinic.api.CourseRecordApi;
-import com.jims.clinic.api.CourseRecordSuperiordocrecordApi;
+import com.jims.clinic.api.CourseRecordSuperiorDocrecorApi;
 import com.jims.clinic.entity.CourseRecord;
 import com.jims.clinic.entity.CourseRecordSuperiordocrecor;
 import com.jims.common.data.StringData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,7 +19,7 @@ import javax.ws.rs.Produces;
 @Path("courseRecordSuperiordocrecor")
 public class CourseRecordSuperiordocrecorRest {
     @Reference(version = "1.0.0")
-    private CourseRecordSuperiordocrecordApi courseRecordSuperiordocrecordApi;
+    private CourseRecordSuperiorDocrecorApi courseRecordSuperiordocrecordApi;
 
     @POST
     @Path("save")
