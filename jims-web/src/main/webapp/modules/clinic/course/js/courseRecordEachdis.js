e019713ff2136d24bdc2f89e3e5ea67b6f11cb21
qinlongxin <443971972@qq.com>
2016-04-25 18:12:35 +0800

    修改病程记录

--- jims-web/src/main/webapp/modules/clinic/course/js/courseRecordEachdis.js
+++ jims-web/src/main/webapp/modules/clinic/course/js/courseRecordEachdis.js
@@ -3,13 +3,13 @@ function saveEachdis() {
     formSubmitInput("eachdisForm");
     $.postForm(basePath + "/courseRecordeachdis/save", "eachdisForm", function (data) {
         if (data.code == "1") {
-            $.messager.alert("��ʾ��Ϣ", "����ɹ�");
-} else {
-    $.messager.alert("��ʾ��Ϣ", "����ʧ��", "error");
-}
+            $.messager.alert("提示信息", "保存成功");
+        } else {
+            $.messager.alert("提示信息", "保存失败", "error");
+        }
 
-}), function (data) {
-    $.messager.alert("��ʾ��Ϣ", "����ʧ��", "error");
-}
+    }), function (data) {
+        $.messager.alert("提示信息", "保存失败", "error");
+    }
 }
 
