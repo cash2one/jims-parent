7fc9658b97395246828b49f39fb7bd77816c0ffc
qinlongxin <443971972@qq.com>
2016-04-26 08:36:44 +0800

    手术申请界面设计

---
+++ jims-web/src/main/webapp/modules/clinic/operationApply/js/operationApply.js
@@ -0,0 +1,14 @@
+
+function savePperationApply() {
+    //formSubmitInput("operationApplyForm");
+    $.postForm(basePath + "/operationApply/save", "operationApplyForm", function (data) {
+        if (data.code == "1") {
+            $.messager.alert("提示信息", "保存成功");
+        } else {
+            $.messager.alert("提示信息", "保存失败", "error");
+        }
+
+    }), function (data) {
+        $.messager.alert("提示信息", "保存失败", "error");
+    }
+}
