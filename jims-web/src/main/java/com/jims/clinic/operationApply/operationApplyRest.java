e17169fdb493af8160b0061cf2007173f8747e98
qinlongxin <443971972@qq.com>
2016-04-26 20:24:32 +0800

    修改病程记录

--- jims-web/src/main/java/com/jims/clinic/operationApply/operationApplyRest.java
+++ jims-web/src/main/java/com/jims/clinic/operationApply/operationApplyRest.java
@@ -1,17 +1,23 @@
 package com.jims.clinic.operationApply;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.google.common.collect.Lists;
-import com.jims.clinic.entity.CourseRecord;
-import com.jims.clinic.entity.CourseRecordEachdis;
-import com.jims.clinic.entity.OutpPresc;
+import com.jims.clinic.api.OperatioinSerivceApi;
+import com.jims.clinic.entity.*;
+import com.jims.common.data.PageData;
 import com.jims.common.data.StringData;
+import com.jims.common.persistence.Page;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,28 +27,52 @@ import java.util.List;
 @Produces("application/json")
 @Path("operationApply")
 public class operationApplyRest {
+    @Reference(version = "1.0.0")
+    private OperatioinSerivceApi operatioinServiceApi ;
+    /**
+     * 异步加载表格
+     * @param request
+     * @param response
+     * @return
+     */
     @Path("list")
     @GET
-    public List<OutpPresc> list(){
-
-        OutpPresc op = new OutpPresc();
-        List<OutpPresc> list = Lists.newArrayList();
-        try {
-//            list = outpPrescServiceApi.findList(outpPresc);
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
+    public PageData list(@Context HttpServletRequest request,@Context HttpServletResponse response){
+        Page<Operatioin> page = operatioinServiceApi.findPage(new Page<Operatioin>(request,response), new Operatioin());
+        PageData pageData=new PageData();
+        pageData.setRows(page.getList());
+        pageData.setTotal(page.getCount());
+        return pageData;
     }
+    /**
+     *
+     * @param ids
+     * @return
+     */
+    @Path("del")
+    @POST
+    public StringData del(String ids){
+        String num=operatioinServiceApi.delete(ids);
+        StringData stringData=new StringData();
+        stringData.setCode(num);
+        stringData.setData("success");
+        return stringData;
+    }
+
     /**
      * 保存每日病程
      */
     @Path("save")
     @POST
-    public StringData save(CourseRecordEachdis courseRecordEachdis) {
+    public StringData save(Operatioin operatioin) {
         StringData data = new StringData();
         String num = data.getCode();
+        OperationGrade operationGrade=new OperationGrade();
+        operationGrade.setOperatioin(operatioin);
+        List<OperationGrade> list=new ArrayList<OperationGrade>();
+        list.add(operationGrade);
+        operatioin.setList(list);
+        num=operatioinServiceApi.saveOperatioin(operatioin);
         data.setCode("1");
         data.setData("success");
         return data;
--- jims-web/src/main/webapp/modules/clinic/operationApply/js/operationApply.js
+++ jims-web/src/main/webapp/modules/clinic/operationApply/js/operationApply.js
@@ -1,4 +1,65 @@
 
+function onloadMethod(){
+    $('#list_data').datagrid({
+        iconCls:'icon-edit',//图标
+        width: 'auto',
+        height: 'auto',
+        nowrap: false,
+        striped: true,
+        border: true,
+        method:'get',
+        collapsible:false,//是否可折叠的
+        fit: true,//自动大小
+        url:basePath+'/operationApply/list',
+        remoteSort:false,
+        idField:'fldId',
+        singleSelect:false,//是否单选
+        pagination:true,//分页控件
+        pageSize:15,
+        pageList: [10,15,30,50],//可以设置每页记录条数的列表
+        columns:[[      //每个列具体内容
+            {field:'mazuifangfa',title:'麻醉方法',width:'18%',align:'center'},
+            {field:'shoushuDoctor',title:'手术医师',width:'18%',align:'center'},
+            {field:'shoushuDate',title:'手术时间',width:'20%',align:'center'},
+            {field:'id',title:'操作',width:'40%',align:'center',formatter:function(value, row, index){
+                var html='<button class="easy-nbtn easy-nbtn-info easy-nbtn-s" onclick="get(\''+row.id+'\',\''+row.type+'\')"><img src="/static/images/index/icon2.png"  width="12" />修改</button>'+
+                    '<button class="easy-nbtn easy-nbtn-warning easy-nbtn-s" onclick="deleteRow(\''+value+'\')"><img src="/static/images/index/icon3.png" width="16"/>删除</button>';
+                return html;
+            }}
+        ]],
+        frozenColumns:[[
+            {field:'ck',checkbox:true}
+        ]],
+        toolbar: [{
+            text: '修改',
+            iconCls: 'icon-edit',
+            handler: function() {
+                var selectRows = $('#list_data').datagrid("getSelections");
+                if (selectRows.length < 1) {
+                    $.messager.alert("提示消息", "请选中需要修改的数据");
+                    return;
+                }
+                get(selectRows[0].id);
+            }
+        }, '-',{
+            text: '删除',
+            iconCls: 'icon-remove',
+            handler: function(){
+                doDelete();
+            }
+        }]
+    });
+    //设置分页控件
+    var p = $('#list_data').datagrid('getPager');
+
+    $("#childrenType").combobox({
+        onChange: function (n,o) {
+            $("#childrenDiv").load(getHtmlPath(n));
+        }
+    });
+}
+
+
 function savePperationApply() {
     //formSubmitInput("operationApplyForm");
     $.postForm(basePath + "/operationApply/save", "operationApplyForm", function (data) {
