5b3f1c7197985d1288017d5adf1a9dd705927b52
tangxinbo <xinliang1230@qq.com>
2016-04-28 14:23:45 +0800

    [txb]菜单管理维护

--- jims-service/src/main/java/com/jims/sys/dao/MenuDictDao.java
+++ jims-service/src/main/java/com/jims/sys/dao/MenuDictDao.java
@@ -8,6 +8,8 @@ import com.jims.common.persistence.CrudDao;
 import com.jims.common.persistence.annotation.MyBatisDao;
 import com.jims.sys.entity.MenuDict;
 
+import java.util.List;
+
 /**
  * 系统菜单DAO接口
  * @author yangruidong
@@ -15,5 +17,14 @@ import com.jims.sys.entity.MenuDict;
  */
 @MyBatisDao
 public interface MenuDictDao extends CrudDao<MenuDict> {
-	
+    /**
+     * 保存方法（返回id）
+     * @param menuDict
+     */
+    public Integer insertReturnObject(MenuDict menuDict);
+    /**
+     * 修改方法（返回id）
+     * @param menuDict
+     */
+    public Integer updateReturnObject(MenuDict menuDict);
 }
