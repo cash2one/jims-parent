ba1e0e9d4c249d33b11de604b1cbb75aace0931d
git1-xueyx <1264073776@qq.com>
2016-04-28 09:24:54 +0800

    会诊记录

--- jims-service/src/main/java/com/jims/clinic/service/ElectronGroupConsultationInService.java
+++ jims-service/src/main/java/com/jims/clinic/service/ElectronGroupConsultationInService.java
@@ -13,6 +13,8 @@ import com.jims.common.service.impl.CrudImplService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * 参与会诊信息Service
  * @author zhaoning
@@ -31,8 +33,61 @@ public class ElectronGroupConsultationInService extends CrudImplService<Electron
      *@return 相关主表的全部信息ElectronGroupConsultationIn.ElectronGroupConsultation
      * 包括主表及其相关的所有的子表（ElectronGroupConsultationIn）信息List
      */
-    @Override
+    //@Override
     public ElectronGroupConsultationIn getByMain(ElectronGroupConsultation electronGroupConsultation) {
-        return dao.getConsultByMain(electronGroupConsultation.getId());
+        return dao.getByMain(electronGroupConsultation.getId());
+    };
+
+    /**异步参与医生信息列表
+     * 查询主表的所有的子表（ElectronGroupConsultationIn）信息List
+     * @param “a.huizhen_id=#{id}”
+     * @author xueyx
+     * @version 2016/4/22
+     */
+    //@Override
+    public List<ElectronGroupConsultationIn> getListByMain(ElectronGroupConsultation electronGroupConsultation){
+        return dao.getListByMain(electronGroupConsultation);
+    };
+
+    /**异步参与医生信息列表
+     * 查询所属同一主表的所有的子表（ElectronGroupConsultationIn）信息List
+     * @param “ElectronGroupConsultationIn.id”
+     * @author xueyx
+     * @version 2016/4/22
+     */
+    //@Override
+    public List<ElectronGroupConsultationIn> getBrotherList(ElectronGroupConsultationIn electronGroupConsultationIn){
+        return dao.getBrotherList(electronGroupConsultationIn);
+    }
+
+    /**
+     * 查询信息
+     * @param-ElectronGroupConsultationIn.doctorId
+     * @param-ElectronGroupConsultationIn.huizhenId
+     * @return 需要返回主表
+     */
+    public ElectronGroupConsultationIn getByMainIdAndDoctorId(ElectronGroupConsultationIn electronGroupConsultationIn){
+        return dao.getByMainIdAndDoctorId(electronGroupConsultationIn);
+    }
+
+    /**
+     * 保存参与会诊人的意见
+     * @param "ElectronGroupConsultationIn.id"
+     * @param "ElectronGroupConsultationIn.inHuizhenyijian "
+     * @author xueyx
+     * @version 2016-04-26
+     */
+    public void saveOtherIdea(ElectronGroupConsultationIn electronGroupConsultationIn){
+        dao.saveOtherIdea(electronGroupConsultationIn);
+    }
+
+    /**
+     * 删除主表相关的子表
+     * @param "electronGroupConsultation.id"
+     * @author xueyx
+     * @version 2016-04-26
+     */
+    public void delByMain(ElectronGroupConsultation electronGroupConsultation){
+        dao.delByMain(electronGroupConsultation);
     }
 }
