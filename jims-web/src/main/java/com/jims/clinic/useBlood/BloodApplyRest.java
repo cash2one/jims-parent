e2e8b424ffd399a337c0a8be1ded6ae38f29e3ac
qinlongxin <443971972@qq.com>
2016-04-29 16:03:49 +0800

    修改病程记录

---
+++ jims-web/src/main/java/com/jims/clinic/useBlood/BloodApplyRest.java
@@ -0,0 +1,16 @@
+package com.jims.clinic.useBlood;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by qinlongxin on 2016/4/29.
+ */
+@Component
+@Produces("application/json")
+@Path("bloodApply")
+public class BloodApplyRest {
+
+}
