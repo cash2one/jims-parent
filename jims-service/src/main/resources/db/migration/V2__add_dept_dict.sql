11e46477119b2e1fe7a13f6b4bffb5b2d63ecca9
yangruidong <18745200375@126.com>
2016-04-29 13:49:38 +0800

    科室和科室属性信息维护

--- jims-service/src/main/resources/db/migration/V2__add_dept_dict.sql
+++ jims-service/src/main/resources/db/migration/V2__add_dept_dict.sql
@@ -1,7 +1,7 @@
                  create table DEPT_DICT
 (
    ID                   VARCHAR2(64)         not null,
-   PAREN_IDT            VARCHAR2(64),
+   PARENT_ID            VARCHAR2(64),
    DEPT_NAME            VARCHAR2(100),
    DEPT_CODE            VARCHAR(20),
    DEPT_PROPERTITY      VARCHAR(100),
@@ -31,3 +31,23 @@ comment on column DEPT_DICT.DEPT_CODE is
 
 comment on column DEPT_DICT.DEPT_PROPERTITY is
 '科室属性';
+
+
+
+insert into DEPT_DICT (ID, PARENT_ID, DEPT_NAME, DEPT_CODE, DEPT_PROPERTITY, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE)
+values ('5a23e97261fd4d41b25eb90f49bbb7ed', null, '门诊', '0101011', '2;0;', null, null, null, to_date('29-04-2016 11:18:21', 'dd-mm-yyyy hh24:mi:ss'), '0', to_date('29-04-2016 11:18:21', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into DEPT_DICT (ID, PARENT_ID, DEPT_NAME, DEPT_CODE, DEPT_PROPERTITY, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE)
+values ('70c139ee2a2c42ae9ad6e8aa1937f3bf', null, '住院部', '02020220', '8;1;', null, null, null, to_date('29-04-2016 11:49:39', 'dd-mm-yyyy hh24:mi:ss'), '0', to_date('29-04-2016 11:19:26', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into DEPT_DICT (ID, PARENT_ID, DEPT_NAME, DEPT_CODE, DEPT_PROPERTITY, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE)
+values ('a5c16628451342f180c5baa9dfb7c6cd', '5a23e97261fd4d41b25eb90f49bbb7ed', '住院', '030303', ';;', null, null, null, to_date('29-04-2016 11:51:51', 'dd-mm-yyyy hh24:mi:ss'), '0', to_date('29-04-2016 11:51:42', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into DEPT_DICT (ID, PARENT_ID, DEPT_NAME, DEPT_CODE, DEPT_PROPERTITY, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE)
+values ('fb244be624a44528bf124b4f1016d184', '70c139ee2a2c42ae9ad6e8aa1937f3bf', '1111', '1111', '2;;', null, null, null, to_date('29-04-2016 12:04:09', 'dd-mm-yyyy hh24:mi:ss'), '0', to_date('29-04-2016 11:52:56', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into DEPT_DICT (ID, PARENT_ID, DEPT_NAME, DEPT_CODE, DEPT_PROPERTITY, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE)
+values ('131f053495e840c693383b64550333d2', 'fb244be624a44528bf124b4f1016d184', '临床', '010101', '8;;', null, null, null, to_date('29-04-2016 12:00:28', 'dd-mm-yyyy hh24:mi:ss'), '0', to_date('29-04-2016 12:00:28', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into DEPT_DICT (ID, PARENT_ID, DEPT_NAME, DEPT_CODE, DEPT_PROPERTITY, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE)
+values ('fe4813e675cb47539e9f9e99e491142e', '5a23e97261fd4d41b25eb90f49bbb7ed', '门诊', '535', '3;0;', null, null, null, to_date('29-04-2016 12:05:19', 'dd-mm-yyyy hh24:mi:ss'), '0', to_date('29-04-2016 12:05:19', 'dd-mm-yyyy hh24:mi:ss'));
--- jims-web/src/main/java/com/jims/sys/DeptDictRest.java
+++ jims-web/src/main/java/com/jims/sys/DeptDictRest.java
@@ -2,17 +2,22 @@ package com.jims.sys;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.jims.common.data.StringData;
+import com.jims.common.utils.StringUtils;
 import com.jims.sys.api.DeptDictApi;
+import com.jims.sys.api.DeptPropertyDictApi;
 import com.jims.sys.entity.DeptDict;
 import com.jims.sys.entity.Dict;
+import com.jims.sys.entity.OrgDeptPropertyDict;
 import com.jims.sys.entity.SysCompany;
+import com.jims.sys.vo.DeptDictVo;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Administrator on 2016/4/24 0024.
+ * Created by yangruidong on 2016/4/24 0024.
  */
 @Component
 @Produces("application/json")
@@ -21,18 +26,55 @@ public class DeptDictRest {
     @Reference(version = "1.0.0")
     private DeptDictApi deptDictApi;
 
+    @Reference(version = "1.0.0")
+    private DeptPropertyDictApi deptPropertyDictApi;
 
-
+    /**
+     * 查询所有的科室信息
+     *
+     * @return
+     */
     @Path("list")
     @GET
-    public List<DeptDict> merge() {
+    public List<DeptDict> list() {
 
+        //查询出所有的科室信息
         List<DeptDict> list = deptDictApi.findAllList();
+
+        //查询出所有的科室属性的类型
+        List<OrgDeptPropertyDict> listProperty = deptPropertyDictApi.findProperty();
+
+        //遍历所有的科室信息
+        for (int i = 0; i < list.size(); i++) {
+            StringBuilder sb = new StringBuilder();
+            //得到每一个对象的科室属性，以；进行切割
+            String[] str = list.get(i).getDeptPropertity().split(";");
+            //遍历获得的数组
+            for (int y = 0; y < str.length; y++) {
+                //得到每一个切割后的科室属性值
+                if (StringUtils.isNotBlank(str[y])) {
+                    //拿科室属性值和科室的类型去数据库中查询科室属性名称
+                    OrgDeptPropertyDict listName = deptPropertyDictApi.findNameByTypeAndValue(listProperty.get(y).getPropertyType(), str[y]);
+                    if (listName == null) {
+                        sb.append("");
+                    } else {
+
+                        sb.append(listName.getPropertyName() + " ");
+                    }
+                }
+            }
+           // sb.deleteCharAt(sb.length() - 1);
+            list.get(i).setDeptPropertity(sb.toString());
+
+        }
         return list;
     }
 
-
-
+    /**
+     * 查询所有的上级科室
+     *
+     * @return
+     */
     @Path("selectParent")
     @POST
     public List<DeptDict> findParent() {
@@ -49,37 +91,40 @@ public class DeptDictRest {
      */
     @Path("add")
     @POST
-    public StringData save(DeptDict deptDict) {
+    public StringData save(DeptDictVo deptDictVo) {
 
-       /* int num = deptDictApi.add(deptDict);
-        if(num!=0)
-        {
-            StringData stringData = new StringData();
-            stringData.setData("success");
-            return stringData;
+        System.out.print(deptDictVo);
+        DeptDict deptDict = new DeptDict();
+        deptDict.setParentId(deptDictVo.getParentId());
+
+        deptDict.setId(deptDictVo.getId());
+
+        deptDict.setDeptCode(deptDictVo.getDeptCode());
+        deptDict.setDeptName(deptDictVo.getDeptName());
+
+        StringBuilder sb = new StringBuilder();
+        String deptPropertity[] = deptDictVo.getArray();
+        for (int i = 0; i < deptPropertity.length; i++) {
+
+                sb.append(deptPropertity[i] + ";");
         }
-           return null;*/
-        System.out.print(deptDict.getId());
-        String num= deptDictApi.save(deptDict);
+
+        deptDict.setDeptPropertity(sb.toString());
+
+
+        String num = deptDictApi.save(deptDict);
         StringData stringData = new StringData();
         stringData.setData("success");
         return stringData;
     }
 
-    @POST
-    @Path("update")
-    public StringData update(DeptDict deptDict) {
-        System.out.print(deptDict.getId());
-        int num = deptDictApi.update(deptDict);
-        if (num != 0) {
-            StringData stringData = new StringData();
-            stringData.setData("success");
-            return stringData;
-        }
-        return null;
 
-    }
-
+    /**
+     * 删除科室信息
+     *
+     * @param ids
+     * @return
+     */
     @Path("del")
     @POST
     public StringData del(String ids) {
