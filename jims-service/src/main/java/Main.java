66da6ccbafa6382b2cbadc6d668da0fb109d28aa
tangxinbo <xinliang1230@qq.com>
2016-05-04 11:52:56 +0800

    [tangxinbo]      1.删除ExamClassDictRest类

--- jims-service/src/main/java/Main.java
+++ jims-service/src/main/java/Main.java
@@ -52,16 +52,16 @@ public class Main {
 
         Flyway flyway = new Flyway();
         //设置数据库链接配置
-        flyway.setDataSource("jdbc:oracle:thin:@127.0.0.1:1521:oracle","his","his");
+        flyway.setDataSource("jdbc:oracle:thin:@127.0.0.1:1521:slhislocal","wx","wx");
         //设置schema用户
-        flyway.setSchemas("HIS");
+        flyway.setSchemas("WX");
         flyway.setTable("SCHEMA_VERSION");
         flyway.setEncoding("UTF-8");
         flyway.setValidateOnMigrate(true);
         //清空所有表结构
-        flyway.clean();
+//        flyway.clean();
         //初始化flyWAy
-        flyway.init();
+//        flyway.init();
         //执行版本控制
         flyway.migrate();
 
