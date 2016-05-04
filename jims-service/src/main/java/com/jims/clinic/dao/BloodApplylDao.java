491da747dae82553a7323e0db1bb1ee402df7e54
qinlongxin <443971972@qq.com>
2016-05-04 18:25:29 +0800

    修改用血申请

--- jims-service/src/main/java/com/jims/clinic/dao/BloodApplylDao.java
+++ jims-service/src/main/java/com/jims/clinic/dao/BloodApplylDao.java
@@ -14,6 +14,6 @@ import com.jims.common.persistence.annotation.MyBatisDao;
  * @version 2016-04-28
  */
 @MyBatisDao
-public interface BloodApplylDao extends CrudDao<BloodApply> {
+public interface BloodApplylDao  extends CrudDao<BloodApply> {
 	
 }
