4b9b92b85eb5741796d213748880660ee5458d19
ligx100 <aff>
2016-05-05 14:26:54 +0800

    [lgx]      1 临床诊疗项目功能

--- jims-api/src/main/java/com/jims/clinic/api/ClinicItemApi.java
+++ jims-api/src/main/java/com/jims/clinic/api/ClinicItemApi.java
@@ -28,12 +28,6 @@ public interface ClinicItemApi {
     public List<ClinicItemDict> findList(ClinicItemDict entity);
 
     /**
-     * 编码或名称是否已存在
-     * @return
-     */
-    public boolean codeOrNameHas(ClinicItemDict entity);
-
-    /**
      * 查询临床诊疗项目分页数据
      * @param page 分页对象
      * @param entity
@@ -158,6 +152,6 @@ public interface ClinicItemApi {
      */
     public String deleteVs(ClinicItemDict entity);
 
-    public String test();
+
 
 }
