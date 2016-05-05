67e931224b459e1080abf3f94f4a1ac4ddec6c77
zhangyao <83221330@qq.com>
2016-05-05 18:32:03 +0800

    修改病程时间

--- jims-api/src/main/java/com/jims/clinic/entity/CourseRecordStage.java
+++ jims-api/src/main/java/com/jims/clinic/entity/CourseRecordStage.java
@@ -1,7 +1,11 @@
 package com.jims.clinic.entity;
 
 import com.jims.common.persistence.DataEntity;
+import com.jims.common.utils.CustomDateDeSerializer;
+import com.jims.common.utils.CustomDateSerializer;
 import com.jims.common.utils.DateUtils;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.hibernate.validator.constraints.Length;
 
 import javax.xml.bind.annotation.XmlRootElement;
@@ -34,17 +38,15 @@ public class CourseRecordStage extends DataEntity<CourseRecordStage> {
         this.lasttime = lasttime;
     }
 
+    @JsonSerialize(using = CustomDateSerializer.class)
     public Date getNowtime() {
         return nowtime;
     }
-
-//    public void setNowtime(Date nowtimes) {
-//        this.nowtime = nowtimes;
-//    }
-
-    public void setNowtime(String nowtime) {
-        this.nowtime =DateUtils.parseDate(nowtime);
+    @JsonDeserialize(using = CustomDateDeSerializer.class)
+    public void setNowtime(Date nowtimes) {
+        this.nowtime = nowtimes;
     }
+
 
     public CourseRecordStage() {
         super();
@@ -132,14 +134,14 @@ public class CourseRecordStage extends DataEntity<CourseRecordStage> {
     }
 
 
+    @JsonSerialize(using = CustomDateSerializer.class)
     public Date getLuruShijian() {
         return luruShijian;
     }
-
-    public void setLuruShijian(String luruShijian) {
-        this.luruShijian=DateUtils.parseDate(luruShijian);
+    @JsonDeserialize(using = CustomDateDeSerializer.class)
+    public void setLuruShijian(Date luruShijian) {
+        this.luruShijian= luruShijian;
     }
-
     public String getType() {
         return type;
     }
