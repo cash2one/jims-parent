a04d727e3874931a658189bb9b058add094ddf33
git1-xueyx <1264073776@qq.com>
2016-05-07 14:00:54 +0800

    检验表整理 xueyx

--- jims-service/src/main/java/com/jims/sys/dao/LabItemClassDictDao.java
+++ jims-service/src/main/java/com/jims/sys/dao/LabItemClassDictDao.java
@@ -5,21 +5,22 @@ package com.jims.sys.dao;
 
 import com.jims.common.persistence.CrudDao;
 import com.jims.common.persistence.annotation.MyBatisDao;
-import com.jims.sys.entity.SpecimanDict;
+import com.jims.sys.entity.LabItemClassDict;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 /**
- * 标本字典DAO接口
+ * 检验项目类别字典DAO接口
  * @author xueyx
- * @version 2016-05-04
+ * @version 2016-05-05
  */
 @MyBatisDao
-public interface SpecimanDictDao extends CrudDao<SpecimanDict> {
+public interface LabItemClassDictDao extends CrudDao<LabItemClassDict> {
+
     /**
-     * 查询科室代码下的检验标本
+     * 查询科室代码下的检验类别
      * @return
      */
-    public List<SpecimanDict> findListByDeptCode(@Param("deptCode")String deptCode);
+    public List<LabItemClassDict> findListByDeptCode(@Param("deptCode")String deptCode);
 }
