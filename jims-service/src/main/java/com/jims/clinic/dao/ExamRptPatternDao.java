33f674d083af1d4c30ceab99068b6022b3ec356d
Ellis Zhao <zhaotieqiang@gmail.com>
2016-05-09 11:09:54 +0800

    【ztq】 1，修改检查项目同步问题。

--- jims-service/src/main/java/com/jims/clinic/dao/ExamRptPatternDao.java
+++ jims-service/src/main/java/com/jims/clinic/dao/ExamRptPatternDao.java
@@ -18,4 +18,12 @@ import java.util.List;
 @MyBatisDao
 public interface ExamRptPatternDao extends CrudDao<ExamRptPattern> {
 	public List getExamRptPattern(String examSubClass);
+    /**
+     * 获取检查项目通过类别
+     * @param orgId 机构id
+     * @param className 父类别
+     * @param subClassName 子类别
+     * @return
+     */
+    public List<ExamRptPattern> listByClass(String orgId,String className,String subClassName);
 }
