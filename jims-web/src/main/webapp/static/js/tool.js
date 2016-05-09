b35c51f697e6dffe46f179579681d46694142329
zhangyao <83221330@qq.com>
2016-05-09 16:22:15 +0800

    删除无用变量

--- jims-web/src/main/webapp/static/js/tool.js
+++ jims-web/src/main/webapp/static/js/tool.js
@@ -387,6 +387,3 @@ $.fn.datetimebox.defaults.parser = function(s){
     var _ae5=parseInt(tt[2],10)||0;
     return new Date(d.getFullYear(),d.getMonth(),d.getDate(),hour,_ae4,_ae5);
 };
-
-var config = {} ;
-config.org_id="1234567" ;
