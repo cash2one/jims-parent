9ec13fd6bdc7b7116b07e040e388ea9f95b9045a
fyg408224345 <408224345@qq.com>
2016-05-10 07:58:21 +0800

    字典Dao接口 DictDao.java

--- jims-service/src/main/java/com/jims/sys/dao/DictDao.java
+++ jims-service/src/main/java/com/jims/sys/dao/DictDao.java
@@ -18,6 +18,21 @@ import java.util.List;
 @MyBatisDao
 public interface DictDao extends CrudDao<Dict> {
 
+    /**
+     * 查询字典表的类型和描述这两个字段
+     * @return 类型描述的列表list集合
+     * @author fengyuguang
+     */
+    public List<Dict> leftList();
+
+    /**
+     * 根据字典表的类型查询属于该类型的数据列表
+     * @param type 字典表类型
+     * @return 字典表某类的列表集合
+     * @author fengyuguang
+     */
+    public List<Dict> rightList(String type);
+
 	public List<String> findTypeList(Dict dict);
 
     public List<String> findListType(String dict);
