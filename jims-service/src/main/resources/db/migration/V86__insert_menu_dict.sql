571a4bb38da012cb3b3a92eb251322a903ebce46
zhu <964483087@qq.com>
2016-05-12 09:21:49 +0800

    [朱齐] 1.药类维护前台页面实现 2.sql增加菜单按钮 3.xml update 中增加删除状态字段修改

--- jims-service/src/main/resources/db/migration/V86__insert_menu_dict.sql
+++ jims-service/src/main/resources/db/migration/V86__insert_menu_dict.sql
@@ -4,6 +4,7 @@
 /* CREATE_BY :  朱齐                                              */
 /*==============================================================*/
 -- insert into
+
 insert into menu_dict (ID, MENU_NAME, HREF, ICON, SORT, TARGET, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE, PID, MENU_LEVEL)
 values ('3154CD0A37344E4BBF3B49A00B2C77DF', '药品类别管理', '/modules/phstock/drug-class-dict.html', '1', 2, '1', null, null, null, '11-5月 -16 03.11.18.646000 下午', '0', '11-5月 -16 03.11.18.646000 下午', '9ED7DB110B4F41F7AED1340F9B26CF1C', '1');
 
