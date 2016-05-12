90070eae23616139cb12ce638ddd7838db08f201
yangruidong <18745200375@126.com>
2016-05-12 09:46:16 +0800

    添加组织机构人员的菜单

---
+++ jims-service/src/main/resources/db/migration/V86__add_data_menu_dict.sql
@@ -0,0 +1,8 @@
+
+
+-- insert data  Menu_dict
+/*==============================================================*/
+/* Table: Menu_dict          给组织机构人员加菜单     createBy  yangruidong    */
+/*==============================================================*/
+insert into MENU_DICT (id, menu_name, href, icon, sort, target, remarks, update_by, create_by, update_date, del_flag, create_date, pid, menu_level)
+values ('AA68F77546214C038A1F1833365F9123', '组织机构人员管理', '/modules/sys/orgStaff.html', null, 1, '1', null, null, null, to_timestamp('12-05-2016 09:41:48.960000', 'dd-mm-yyyy hh24:mi:ss.ff'), '0', to_timestamp('12-05-2016 09:41:48.960000', 'dd-mm-yyyy hh24:mi:ss.ff'), '55CB0105F69B490DA48F7D1F1029370A', '1');
