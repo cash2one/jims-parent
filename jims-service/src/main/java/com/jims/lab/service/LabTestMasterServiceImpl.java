396da57c2f18de08aa2502c7af8a7ff737cacbe0
git1-xueyx <1264073776@qq.com>
2016-05-16 10:41:26 +0800

    检验修改      xueyx

--- jims-service/src/main/java/com/jims/lab/service/LabTestMasterServiceImpl.java
+++ jims-service/src/main/java/com/jims/lab/service/LabTestMasterServiceImpl.java
@@ -222,7 +222,7 @@ public class LabTestMasterServiceImpl  extends CrudImplService<LabTestMasterDao,
                 orders.setPatientId(labTestMaster.getPatientId());
                 orders.setVisitId(labTestMaster.getVisitId().toString());
                 orders.setOrderNo(ordersDao.creeatOrderNo(orders));
-                orders.setOrderSubNo(Long.valueOf(i+1));
+                orders.setOrderSubNo(Long.valueOf(1));
                 orders.setStartDateTime(labTestMaster.getRequestedDateTime());
                 orders.setRepeatIndicator("0");
                 orders.setOrderClass("C");
