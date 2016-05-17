902d7dce4600de99ff6641b54e29f293ecd35679
pangqian <1525801249@qq.com>
2016-05-17 12:53:31 +0800

    修改sql

--- jims-service/src/main/resources/db/migration/V120__alter_pats_in_hospital.sql
+++ jims-service/src/main/resources/db/migration/V120__alter_pats_in_hospital.sql
@@ -8,9 +8,9 @@ alter table PATS_IN_HOSPITAL add ORG_ID VARCHAR2(64);
 alter table PATS_IN_HOSPITAL add CLINIC_ID VARCHAR2(64);
 alter table PATS_IN_HOSPITAL add PATIENT_ID VARCHAR2(64);
 -- Add comments to the columns 
-comment on column SCHEDULED_OPERATION_NAME.PATIENT_ID
+comment on column PATS_IN_HOSPITAL.PATIENT_ID
   is '组织机构';
-comment on column SCHEDULED_OPERATION_NAME.VISIT_ID
+comment on column PATS_IN_HOSPITAL.CLINIC_ID
   is '就诊ID';
-comment on column SCHEDULED_OPERATION_NAME.VISIT_ID
+comment on column PATS_IN_HOSPITAL.PATIENT_ID
  is '病人ID';
