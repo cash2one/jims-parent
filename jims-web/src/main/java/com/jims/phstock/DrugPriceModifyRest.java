056d47b48ce69fa203d11f706f8103cf99845e2a
tangxinbo <xinliang1230@qq.com>
2016-05-19 08:55:20 +0800

    [txb]    1.药品调价维护功能完善

--- jims-web/src/main/java/com/jims/phstock/DrugPriceModifyRest.java
+++ jims-web/src/main/java/com/jims/phstock/DrugPriceModifyRest.java
@@ -23,12 +23,24 @@ public class DrugPriceModifyRest {
     @Reference(version = "1.0.0")
     private DrugPriceListServiceApi drugPriceListServiceApi;
 
+    /**
+     * 查询列表药品调价数据
+     * @return
+     * @author txb
+     * @version 2016-05-18
+     */
     @Path("findAll")
     @GET
     public List<DrugPriceModify> findAllList() {
         return drugPriceListServiceApi.findListDrugPriceModify(new DrugPriceModify());
     }
-
+    /**
+     * 保存药品调价数据
+     * @param drugPriceModifyVo
+     * @return
+     * @author txb
+     * @version 2016-05-18
+     */
     @Path("save")
     @POST
     public StringData save(DrugCatalogChangeVo<DrugPriceModify> drugPriceModifyVo) {
