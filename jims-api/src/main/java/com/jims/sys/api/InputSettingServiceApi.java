fbd8c6fe87d5f490439287753871d5f9031dd5f6
yangruidong <18745200375@126.com>
2016-05-20 08:43:39 +0800

    输入法字典维护

--- jims-api/src/main/java/com/jims/sys/api/InputSettingServiceApi.java
+++ jims-api/src/main/java/com/jims/sys/api/InputSettingServiceApi.java
@@ -1,6 +1,9 @@
 package com.jims.sys.api;
 
 import com.jims.common.web.impl.BaseDto;
+import com.jims.sys.entity.InputSettingDetail;
+import com.jims.sys.entity.InputSettingMaster;
+import com.jims.sys.vo.InputSettingVo;
 
 import java.util.List;
 
@@ -26,5 +29,40 @@ public interface InputSettingServiceApi {
      * @return
      */
     public List<String> listTableColByTableName(String tableName) ;
+
+    /**
+     * 根据组织机构id查询输入法字典的全部信息
+     * @param orgId 组织机构id
+     * @return
+     * @author yangruidong
+     */
+    public List<InputSettingMaster> findAllListByOrgId(String orgId);
+
+
+    /**
+     * 保存  增删改
+     * @param inputSettingMasterVo
+     * @return
+     *  @author  yangruidong
+     */
+    public List<InputSettingMaster> saveAll(InputSettingVo<InputSettingMaster> inputSettingMasterVo);
+
+
+    /**
+     * 根据输入法主记录的id查询输入法字典明细表的信息
+     * @param id
+     * @return
+     * @author yangruidong
+     */
+    public List<InputSettingDetail> findListDetail(String id);
+
+    /**
+     * 保存  增删改  输入法字典明细表
+     * @param inputSettingDetailVo
+     * @return
+     *  @author  yangruidong
+     */
+    public List<InputSettingDetail> saveDetail(InputSettingVo<InputSettingDetail> inputSettingDetailVo);
+
 
 }
