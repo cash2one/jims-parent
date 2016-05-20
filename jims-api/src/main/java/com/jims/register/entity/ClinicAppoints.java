8194f8af13aebd239562b9215a5527fecdf74d49
zhangyao <83221330@qq.com>
2016-05-20 14:17:36 +0800

    增加预约记录

---
+++ jims-api/src/main/java/com/jims/register/entity/ClinicAppoints.java
@@ -0,0 +1,224 @@
+package com.jims.register.entity;
+
+import com.jims.common.persistence.DataEntity;
+
+import java.util.Date;
+
+/**
+ * 预约挂号Entity
+ * @author zhangyao
+ * @version 2016-05-20
+ */
+public class ClinicAppoints extends DataEntity<ClinicAppoints> {
+	
+	private static final long serialVersionUID = 1L;
+	private String orgId;		// 医院ID
+	private Date visitDateAppted;		// 就诊日期
+	private String clinicLabel;		// 号别
+	private String patientId;		// 病人标识号
+	private String visitTimeAppted;		// 预约就诊时间
+	private Date apptMadeDate;		// 何日预约
+	private String modeCode;		// 预约模式
+	private String cardName;		// 卡名
+	private String cardNo;		// 卡号
+	private Integer serialNo;		// 流水号
+	private String name;		// 姓名
+	private String sex;		// 性别
+	private Long age;		// 年龄
+	private String idNo;		// 身份证号
+	private String identity;		// 身份
+	private String chargeType;		// 费别
+	private String insuranceNo;		// 保险号码
+	private String insuranceType;		// 保险类型
+	private String unitInContract;		// 合同单位
+	private String namePhonetic;		// 姓名拼音码
+	
+	public ClinicAppoints() {
+		super();
+	}
+
+	public ClinicAppoints(String id){
+		super(id);
+	}
+
+
+	public String getOrgId() {
+		return orgId;
+	}
+
+	public void setOrgId(String orgId) {
+		this.orgId = orgId;
+	}
+
+
+	public Date getVisitDateAppted() {
+		return visitDateAppted;
+	}
+
+	public void setVisitDateAppted(Date visitDateAppted) {
+		this.visitDateAppted = visitDateAppted;
+	}
+
+
+	public String getClinicLabel() {
+		return clinicLabel;
+	}
+
+	public void setClinicLabel(String clinicLabel) {
+		this.clinicLabel = clinicLabel;
+	}
+
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+
+	public String getVisitTimeAppted() {
+		return visitTimeAppted;
+	}
+
+	public void setVisitTimeAppted(String visitTimeAppted) {
+		this.visitTimeAppted = visitTimeAppted;
+	}
+
+
+	public Date getApptMadeDate() {
+		return apptMadeDate;
+	}
+
+	public void setApptMadeDate(Date apptMadeDate) {
+		this.apptMadeDate = apptMadeDate;
+	}
+
+
+	public String getModeCode() {
+		return modeCode;
+	}
+
+	public void setModeCode(String modeCode) {
+		this.modeCode = modeCode;
+	}
+
+
+	public String getCardName() {
+		return cardName;
+	}
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+
+	public Integer getSerialNo() {
+		return serialNo;
+	}
+
+	public void setSerialNo(Integer serialNo) {
+		this.serialNo = serialNo;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+
+	public String getIdNo() {
+		return idNo;
+	}
+
+	public void setIdNo(String idNo) {
+		this.idNo = idNo;
+	}
+
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+
+
+	public String getChargeType() {
+		return chargeType;
+	}
+
+	public void setChargeType(String chargeType) {
+		this.chargeType = chargeType;
+	}
+
+
+	public String getInsuranceNo() {
+		return insuranceNo;
+	}
+
+	public void setInsuranceNo(String insuranceNo) {
+		this.insuranceNo = insuranceNo;
+	}
+
+
+	public String getInsuranceType() {
+		return insuranceType;
+	}
+
+	public void setInsuranceType(String insuranceType) {
+		this.insuranceType = insuranceType;
+	}
+
+
+	public String getUnitInContract() {
+		return unitInContract;
+	}
+
+	public void setUnitInContract(String unitInContract) {
+		this.unitInContract = unitInContract;
+	}
+
+
+	public String getNamePhonetic() {
+		return namePhonetic;
+	}
+
+	public void setNamePhonetic(String namePhonetic) {
+		this.namePhonetic = namePhonetic;
+	}
+
+}
---
+++ jims-service/src/main/resources/db/migration/V137__add_clinic_appoints .sql
@@ -0,0 +1,115 @@
+/*==============================================================*/
+/* Table: CLINIC_APPOINTS                 门诊预约记录        */
+/*==============================================================*/
+create table CLINIC_APPOINTS
+(
+   ID                   VARCHAR2(64)         not null,
+   ORG_ID               VARCHAR2(64),
+   VISIT_DATE_APPTED    TIMESTAMP,
+   CLINIC_LABEL         VARCHAR2(64),
+   PATIENT_ID           VARCHAR2(64),
+   VISIT_TIME_APPTED    VARCHAR2(64),
+   APPT_MADE_DATE       TIMESTAMP,
+   MODE_CODE            CHAR,
+   CARD_NAME            VARCHAR2(64),
+   CARD_NO              VARCHAR2(20),
+   SERIAL_NO            NUMBER(3),
+   NAME                 VARCHAR2(100 char),
+   SEX                  VARCHAR2(4),
+   AGE                  NUMBER(16),
+   ID_NO                VARCHAR2(64),
+   IDENTITY             VARCHAR2(10),
+   CHARGE_TYPE          VARCHAR2(16),
+   INSURANCE_NO         VARCHAR2(18),
+   INSURANCE_TYPE       VARCHAR2(16),
+   UNIT_IN_CONTRACT     VARCHAR2(11),
+   NAME_PHONETIC        VARCHAR2(16),
+   CREATE_DATE          TIMESTAMP,
+   CREATE_BY            VARCHAR2(64),
+   UPDATE_DATE          TIMESTAMP,
+   UPDATE_BY            VARCHAR2(64),
+   REMARKS              VARCHAR2(200 char),
+   DEL_FLAG             char                 default '0',
+   constraint PK_CLINIC_APPOINTS primary key (ID)
+);
+
+comment on column CLINIC_APPOINTS.ID is
+'主键';
+
+comment on column CLINIC_APPOINTS.ORG_ID is
+'医院ID';
+
+comment on column CLINIC_APPOINTS.VISIT_DATE_APPTED is
+'就诊日期';
+
+comment on column CLINIC_APPOINTS.CLINIC_LABEL is
+'号别';
+
+comment on column CLINIC_APPOINTS.PATIENT_ID is
+'病人标识号';
+
+comment on column CLINIC_APPOINTS.VISIT_TIME_APPTED is
+'预约就诊时间';
+
+comment on column CLINIC_APPOINTS.APPT_MADE_DATE is
+'何日预约';
+
+comment on column CLINIC_APPOINTS.MODE_CODE is
+'预约模式';
+
+comment on column CLINIC_APPOINTS.CARD_NAME is
+'卡名';
+
+comment on column CLINIC_APPOINTS.CARD_NO is
+'卡号';
+
+comment on column CLINIC_APPOINTS.SERIAL_NO is
+'流水号';
+
+comment on column CLINIC_APPOINTS.NAME is
+'姓名';
+
+comment on column CLINIC_APPOINTS.SEX is
+'性别';
+
+comment on column CLINIC_APPOINTS.AGE is
+'年龄';
+
+comment on column CLINIC_APPOINTS.ID_NO is
+'身份证号';
+
+comment on column CLINIC_APPOINTS.IDENTITY is
+'身份';
+
+comment on column CLINIC_APPOINTS.CHARGE_TYPE is
+'费别';
+
+comment on column CLINIC_APPOINTS.INSURANCE_NO is
+'保险号码';
+
+comment on column CLINIC_APPOINTS.INSURANCE_TYPE is
+'保险类型';
+
+comment on column CLINIC_APPOINTS.UNIT_IN_CONTRACT is
+'合同单位';
+
+comment on column CLINIC_APPOINTS.NAME_PHONETIC is
+'姓名拼音码';
+
+comment on column CLINIC_APPOINTS.CREATE_DATE is
+'创建日期';
+
+comment on column CLINIC_APPOINTS.CREATE_BY is
+'创建人';
+
+comment on column CLINIC_APPOINTS.UPDATE_DATE is
+'更新日期';
+
+comment on column CLINIC_APPOINTS.UPDATE_BY is
+'更新人';
+
+comment on column CLINIC_APPOINTS.REMARKS is
+'备注信息';
+
+comment on column CLINIC_APPOINTS.DEL_FLAG is
+'删除标志';
