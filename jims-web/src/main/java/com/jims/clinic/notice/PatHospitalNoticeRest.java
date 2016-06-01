cc644781d7053f7bec78bf6ba4b7d4d01e141748
qinlongxin <443971972@qq.com>
2016-06-01 19:37:28 +0800

    修改住院通知单

--- jims-web/src/main/java/com/jims/clinic/notice/PatHospitalNoticeRest.java
+++ jims-web/src/main/java/com/jims/clinic/notice/PatHospitalNoticeRest.java
@@ -2,14 +2,15 @@ package com.jims.clinic.notice;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.jims.clinic.api.PatHospitalNoticeServiceApi;
-import com.jims.clinic.entity.DocOperationApply;
 import com.jims.clinic.entity.PatHospitalNotice;
 import com.jims.common.data.PageData;
+import com.jims.common.data.StringData;
 import com.jims.common.persistence.Page;
 import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -22,7 +23,7 @@ import javax.ws.rs.core.Context;
 @Path("patHospitalNotice")
 public class PatHospitalNoticeRest {
     @Reference(version = "1.0.0")
-    private PatHospitalNoticeServiceApi patHospitalNoticeServiceApi;
+    private PatHospitalNoticeServiceApi patHospitalNoticeServiceApi ;
     /**
      * 异步加载表格
      * @param request
@@ -37,5 +38,43 @@ public class PatHospitalNoticeRest {
         pageData.setRows(page.getList());
         pageData.setTotal(page.getCount());
         return pageData;
+    }
+    /**
+     * 保存申请记录
+     */
+    @Path("save")
+    @POST
+    public StringData save(PatHospitalNotice patHospitalNotice) {
+        StringData data = new StringData();
+        String num = data.getCode();
+        if (patHospitalNotice != null) {
+            num = patHospitalNoticeServiceApi.save(patHospitalNotice);
+        }
+        data.setCode(num);
+        data.setData("success");
+        return data;
+    }
+    /**
+     *删除手术申请
+     * @param ids
+     * @return
+     */
+    @Path("del")
+    @POST
+    public StringData del(String ids){
+        StringData stringData=new StringData();
+        String num=patHospitalNoticeServiceApi.delete(ids);
+        stringData.setCode(num);
+        stringData.setData("success");
+        return stringData;
+    }
+    /**
+     * 根据id查询手术申请表信息
+     */
+    @Path("get")
+    @POST
+    public PatHospitalNotice get(String id) {
+        PatHospitalNotice patHospitalNotice= patHospitalNoticeServiceApi.get(id);
+        return patHospitalNotice;
     }
 }
