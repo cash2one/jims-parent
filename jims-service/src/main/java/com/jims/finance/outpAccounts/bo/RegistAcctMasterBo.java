2718f9ab2cfffd71adcf4ce4eac791e00123f1d4
SunShinGitHub <931324307@qq.com>
2016-06-02 09:21:57 +0800

    确认结账保存方法

--- jims-service/src/main/java/com/jims/finance/outpAccounts/bo/RegistAcctMasterBo.java
+++ jims-service/src/main/java/com/jims/finance/outpAccounts/bo/RegistAcctMasterBo.java
@@ -1,8 +1,12 @@
 package com.jims.finance.outpAccounts.bo;
 
 import com.jims.common.service.impl.CrudImplService;
+import com.jims.finance.outpAccounts.dao.RegistAcctDetailDao;
 import com.jims.finance.outpAccounts.dao.RegistAcctMasterDao;
+import com.jims.finance.outpAccounts.dao.RegistAcctMoneyDao;
+import com.jims.finance.outpAccounts.entity.RegistAcctDetail;
 import com.jims.finance.outpAccounts.entity.RegistAcctMaster;
+import com.jims.finance.outpAccounts.entity.RegistAcctMoney;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -19,8 +23,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistAcctMasterBo extends CrudImplService<RegistAcctMasterDao, RegistAcctMaster> {
     @Autowired
     RegistAcctMasterDao registAcctMasterDao;
+    @Autowired
+    RegistAcctDetailDao registAcctDetailDao;
+    @Autowired
+    RegistAcctMoneyDao registAcctMoneyDao;
+
 
     public String saveMaster(RegistAcctMaster registAcctMaster) {
+        registAcctMaster.preInsert();
+        if(registAcctMaster.getAcctDetails()!=null&&registAcctMaster.getAcctDetails().size()>0){
+            for(int i=0;i<registAcctMaster.getAcctDetails().size();i++){
+                RegistAcctDetail detail = new RegistAcctDetail();
+                detail = registAcctMaster.getAcctDetails().get(i);
+                detail.preInsert();
+                detail.setAcctId(registAcctMaster.getId());
+                detail.setOrgId(registAcctMaster.getOrgId());
+                registAcctDetailDao.insert(detail);
+            }
+            /*for(RegistAcctDetail detail : registAcctMaster.getAcctDetails()){
+                detail.preInsert();
+                detail.setAcctId(registAcctMaster.getId());
+                detail.setOrgId(registAcctMaster.getOrgId());
+
+                registAcctDetailDao.insert(detail);
+            }*/
+        }
+        if(registAcctMaster.getAcctMoneys()!=null&&registAcctMaster.getAcctDetails().size()>0){
+            for(RegistAcctMoney money : registAcctMaster.getAcctMoneys()){
+                money.setAcctId(registAcctMaster.getId());
+                money.setAcctId(registAcctMaster.getId());
+                money.preInsert();
+                registAcctMoneyDao.insert(money);
+            }
+        }
         return String.valueOf(registAcctMasterDao.insert(registAcctMaster));
     }
 }
