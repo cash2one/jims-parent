c846730fe24e9a0a73e6394f32860279734255e2
pangqian <1525801249@qq.com>
2016-06-02 17:07:07 +0800

    护理端-床位信息

---
+++ jims-api/src/main/java/com/jims/nurse/api/BedRecServiceApi.java
@@ -0,0 +1,10 @@
+package com.jims.nurse.api;
+
+/**
+ * 床位记录
+ *
+ * @author PangQian
+ * @date2016/6/2 0002
+ */
+public interface BedRecServiceApi {
+}
---
+++ jims-service/src/main/java/com/jims/nurse/bo/BedRecBo.java
@@ -0,0 +1,10 @@
+package com.jims.nurse.bo;
+
+/**
+ * BedRecBo
+ *
+ * @author PangQian
+ * @date2016/6/2 0002
+ */
+public class BedRecBo {
+}
