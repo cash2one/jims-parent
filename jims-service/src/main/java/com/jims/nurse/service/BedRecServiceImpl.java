c399e8416f9989e260c8453604b467044e298ee6
pangqian <1525801249@qq.com>
2016-06-03 16:13:06 +0800

    护理端-床位信息

--- jims-service/src/main/java/com/jims/nurse/service/BedRecServiceImpl.java
+++ jims-service/src/main/java/com/jims/nurse/service/BedRecServiceImpl.java
@@ -2,10 +2,13 @@ package com.jims.nurse.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.jims.common.persistence.Page;
+import com.jims.common.web.impl.BaseDto;
 import com.jims.nurse.api.BedRecServiceApi;
 import com.jims.nurse.bo.BedRecBo;
 import com.jims.nurse.entity.BedRec;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * BedRecServiceImpl
@@ -17,9 +20,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class BedRecServiceImpl implements BedRecServiceApi {
   @Autowired
     private BedRecBo bedRecBo;
-
+    /**
+     * 查询护士所负责的病区的床位信息
+     * @param bedRecPage
+     * @param bedRec
+     * @author pq
+     * @return
+     */
     public Page<BedRec> findPage(Page<BedRec> bedRecPage,BedRec bedRec){
        return bedRecBo.findPage(bedRecPage,bedRec);
     }
 
+    /**
+     * 保存床位信息
+     * @param bedRecList
+     * @author pq
+     * @return
+     */
+    public String saveBed(List<BedRec> bedRecList){
+    return  bedRecBo.saveBed(bedRecList);
+    }
+
+    /**
+     * 判断 病区 下的床位号的唯一性
+     * @param bedNo
+     * @param wardCode
+     * @author pq
+     * @return
+     */
+    public boolean judgeBedNo(Integer bedNo,String wardCode){
+      return bedRecBo.judgeBedNo(bedNo,wardCode);
+    }
+
+    /**
+     * 删除床位信息
+     * @param ids
+     * @author pq
+     * @return
+     */
+    public String delete(String ids){
+     return bedRecBo.delete(ids);
+    }
+
+
+  /**
+   * 查询病区下所有的床位信息
+   * @param wardCode
+   * @author pq
+   * @return
+   */
+  public List<BaseDto> getAllBed(String wardCode){
+    return  bedRecBo.getAllBed(wardCode);
+  }
 }
---
+++ jims-web/src/main/webapp/modules/nurse/js/bedInfo.js
@@ -0,0 +1,35 @@
+var wardCode='160101';
+$(function(){
+    $('#bedInfo').datagrid({
+        iconCls: 'icon-edit',//图标
+        width: 'auto',
+        height: 'auto',
+        nowrap: false,
+        striped: true,
+        border: true,
+        method: 'GET',
+        collapsible: false,//是否可折叠的
+        fit: true,//自动大小
+        url: basePath + '/bedRec/getAllBed?wardCode='+wardCode,
+        remoteSort: false,
+        idField: 'id',
+        singleSelect: true,//是否单选
+        columns: [[      //每个列具体内容
+            {field: 'bed_no1', title: '床号', width: '10%', align: 'center'},
+            {field: 'name', title: '姓名', width: '10%', align: 'center'},
+            {field: 'sex', title: '性别', width: '10%', align: 'center'},
+            {field: 'age', title: '年龄', width: '10%', align: 'center'},
+            {field: 'visit_id', title: '住院号', width: '10%', align: 'center'},
+            {field: 'diagnosis', title: '诊断', width: '10%', align: 'center'},
+            {field: 'patient_id', title: '病人ID', width: '10%', align: 'center'},
+            {field: 'doctor_in_charge', title: '医生', width: '10%', align: 'center'}
+
+        ]],onClickRow: function (index, row) {//单击行事件
+            window.parent.document.getElementById("centerIframe").src = "/modules/nurse/centerRegion.html?id="+row.id;
+        }
+    });
+
+
+
+
+});
