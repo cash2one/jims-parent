a7ba67278f83f6d9831f8c2685fd8fd8ff184f12
pangqian <1525801249@qq.com>
2016-06-15 11:37:02 +0800

    Merge branch 'master' of https://github.com/jims-cloud/jims-parent into pangqian
    # Conflicts:
    #	jims-service/src/main/java/com/jims/clinic/dao/PatsInHospitalDao.java

--- jims-service/src/main/java/com/jims/clinic/dao/PreDischgedPatsDao.java
+++ jims-service/src/main/java/com/jims/clinic/dao/PreDischgedPatsDao.java
@@ -1,9 +1,12 @@
 package com.jims.clinic.dao;
 
 import com.jims.clinic.entity.PreDischgedPats;
+import com.jims.clinic.vo.PreDischgedPatsVo;
 import com.jims.common.persistence.CrudDao;
 import com.jims.common.persistence.annotation.MyBatisDao;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 出院通知单DAO接口
@@ -19,5 +22,6 @@ public interface PreDischgedPatsDao extends CrudDao<PreDischgedPats> {
      * @return
      */
     public Integer findByPatientId(@Param("patientId")String patientId);
-	
+    public List<PreDischgedPatsVo> findPreDischList(String wardCode);
+    public int delAll(String hospitalId);
 }
