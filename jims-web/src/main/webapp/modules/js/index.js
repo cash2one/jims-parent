4f505aa2b537b37baa5ab75af14ff1804f280692
fyg408224345 <408224345@qq.com>
2016-06-15 14:02:31 +0800

    [冯宇光]: 顶部显示所有服务，服务按名称排序，名称为系统管理的服务排到最后。

--- jims-web/src/main/webapp/modules/js/index.js
+++ jims-web/src/main/webapp/modules/js/index.js
@@ -20,49 +20,43 @@ config.persion_Id=pid;
 config.operator = 'thinkgem';
 config.currentStorage = '1001';
 $(function () {
-    //var orgId = parent.config.org_Id;
     var orgId = config.org_Id;
     var personId = config.persion_Id;
     var staffId = '';   //员工Id
-    var serviceId = []; //员工对应的多个角色下的所有服务(去掉重复的服务)
+    //var serviceId = []; //员工对应的多个角色下的所有服务(去掉重复的服务)
+    var sysService = {};    //名称为系统管理的服务
 
-    //查询员工信息
+    //根据机构ID和人员ID查询员工ID
     $.get(basePath + '/orgStaff/find-staff-by-orgId-personId?persionId=' + personId + '&orgId=' + orgId, function (data) {
         staffId = data.id;
+    });
+    //根据机构ID和人员ID查询该员工在该机构的所有服务
+    $.get(basePath + '/org-service/find-selfServiceList-by-orgId-personId?personId=' + personId + '&orgId=' + orgId,function(data){
+        for(var i=0;i<data.length;i++){
+            if (data[i].serviceName != '系统管理') {
+                $("#menu").append("<li><a id='" + data[i].id + "'>" + data[i].serviceName + "</a></li>");
+                $("#" + data[i].id).on('click', function () {
 
-        //查询员工的服务
-        $.get(basePath + '/orgStaff/find-serviceId-by-staffId?staffId=' + staffId, function (data) {
-            for (var i = 0; i < data.length; i++) {
-                serviceId.push(data[i].serviceId);
-            }
-            for (var i = 0; i < serviceId.length; i++) {
-                serviceId = serviceId.unique2();    //去掉相同的serviceId
-                $.get(basePath + '/org-service/find-selfServiceList-by-id?id=' + serviceId[i], function (data) {
-                    $("#menu").append("<li><a id='" + data.id + "'>" + data.serviceName + "</a></li>");
-                    $("#" + data.id).on('click', function () {
-
-                       var iframe='<iframe width="100%" id="centerIframe" height="99.6%" frameborder="no"  border="0"  ' +
-                           'scrolling="yes" src="/modules/sys/template.html?serviceId='+data.id+'?staffId='+staffId+'"></iframe>'
-
-                       $("#iframe").html('');
-                        $("#iframe").append(iframe);
-
-
-                    });
+                    var iframe = '<iframe width="100%" id="centerIframe" height="99.6%" frameborder="no"  border="0"  ' +
+                        'scrolling="yes" src="/modules/sys/template.html?serviceId=' + this.id + '?staffId=' + staffId + '"></iframe>'
+                    $("#iframe").html('');
+                    $("#iframe").append(iframe);
                 });
             }
-        });
-    });
-
-    //数组元素去重
-    Array.prototype.unique2 = function () {
-        this.sort(); //先排序
-        var res = [this[0]];
-        for (var i = 1; i < this.length; i++) {
-            if (this[i] !== res[res.length - 1]) {
-                res.push(this[i]);
+            if(data[i].serviceName == '系统管理'){
+                sysService.id = data[i].id;
+                sysService.serviceName = data[i].serviceName;
             }
         }
-        return res;
-    }
+
+        $("#menu").append("<li><a id='" + sysService.id + "'>" + sysService.serviceName + "</a></li>");
+        $("#" + sysService.id).on('click', function () {
+
+            var iframe = '<iframe width="100%" id="centerIframe" height="99.6%" frameborder="no"  border="0"  ' +
+                'scrolling="yes" src="/modules/sys/template.html?serviceId=' + sysService.id + '?staffId=' + staffId + '"></iframe>'
+
+            $("#iframe").html('');
+            $("#iframe").append(iframe);
+        });
+    });
 });
