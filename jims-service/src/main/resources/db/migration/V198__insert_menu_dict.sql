4c4a484fb92207a346b7054b42798a00b6e34a27
xian <690101606@qq.com>
2016-06-17 13:03:21 +0800

    [lgx]      1 修改bug      2 添加初始数据

--- jims-service/src/main/resources/db/migration/V198__insert_menu_dict.sql
+++ jims-service/src/main/resources/db/migration/V198__insert_menu_dict.sql
@@ -1,17 +1,17 @@
 insert into menu_dict (ID, MENU_NAME, HREF, ICON, SORT, TARGET, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE, PID, MENU_LEVEL)
-values (sys_guid(), '收费结账', '/modules/finance/outpAccounts/outpChargeCheck.html', '', null, '', '', '', '', '01-6月 -16 10.00.10.466000 上午', '0', '01-6月 -16 10.00.10.466000 上午', '9396B1CBF46D4902B850331A5BF9ABED', '1');
+values ('c667b7033b934f01933731ad50bf4e2e', '收费结账', '/modules/finance/outpAccounts/outpChargeCheck.html', '', null, '', '', '', '', '01-6月 -16 10.00.10.466000 上午', '0', '01-6月 -16 10.00.10.466000 上午', '9396B1CBF46D4902B850331A5BF9ABED', '1');
 
 
 insert into menu_dict (ID, MENU_NAME, HREF, ICON, SORT, TARGET, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE, PID, MENU_LEVEL)
-values (sys_guid(), '住院管理', '/modules/nurse/centerRegion.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '55CB0105F69B490DA48F7D1F1029370A', '1');
+values ('e072e6a3fe744d02b26047af983f45dd', '住院管理', '/modules/nurse/centerRegion.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '55CB0105F69B490DA48F7D1F1029370A', '1');
 
 
 insert into menu_dict (ID, MENU_NAME, HREF, ICON, SORT, TARGET, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE, PID, MENU_LEVEL)
-values (sys_guid(), '角色维护', '/modules/sys/org-role.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '55CB0105F69B490DA48F7D1F1029370A', '1');
+values ('69357a97e49f4f8397f05d858384b817', '角色维护', '/modules/sys/org-role.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '55CB0105F69B490DA48F7D1F1029370A', '1');
 
 
 insert into menu_dict (ID, MENU_NAME, HREF, ICON, SORT, TARGET, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE, PID, MENU_LEVEL)
-values (sys_guid(), '权限分配', '/modules/sys/org-service-menu.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '55CB0105F69B490DA48F7D1F1029370A', '1');
+values ('a96676df95914212828865d13f26f83a', '权限分配', '/modules/sys/org-service-menu.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '55CB0105F69B490DA48F7D1F1029370A', '1');
 
 insert into menu_dict (ID, MENU_NAME, HREF, ICON, SORT, TARGET, REMARKS, UPDATE_BY, CREATE_BY, UPDATE_DATE, DEL_FLAG, CREATE_DATE, PID, MENU_LEVEL)
-values (sys_guid(), '药品盘点', '/modules/phstock/drug-stock-inventory.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '065969698a724445b2c03085aad8dfcd', '1');
+values ('1a6e1a83209e4f99b3a4387fd13d38d0', '药品盘点', '/modules/phstock/drug-stock-inventory.html', '1', 1, '1', '', '', '', '03-6月 -16 09.16.09.473000 上午', '0', '28-4月 -16 09.30.34.000000 上午', '065969698a724445b2c03085aad8dfcd', '1');
