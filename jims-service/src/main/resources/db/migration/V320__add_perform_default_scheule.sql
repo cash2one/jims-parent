ee4baeb1e652b22001bf8e1c8ac3bf3eabb79d2e
Ellis Zhao <zhaotieqiang@gmail.com>
2016-06-21 14:34:50 +0800

    【赵铁强】 1, 添加默认执行时间表 2，添加unit_Type_dict字典数据

---
+++ jims-service/src/main/resources/db/migration/V320__add_perform_default_scheule.sql
@@ -0,0 +1,22 @@
+-- Create table
+create table PERFORM_DEFAULT_SCHEDULE
+(
+  ID        varchar2(64),
+  FREQ_DESC        VARCHAR2(16),--外键与频次字典表
+  ADMINISTRATION   VARCHAR2(16),--途径
+  DEFAULT_SCHEDULE VARCHAR2(16)--默认执行时间
+);
+-- Add comments to the table
+comment on table PERFORM_DEFAULT_SCHEDULE
+  is '医嘱执行缺省时间表';
+-- Add comments to the columns
+comment on column PERFORM_DEFAULT_SCHEDULE.ID
+  is '序号';
+comment on column PERFORM_DEFAULT_SCHEDULE.FREQ_DESC
+  is '执行频率描述';
+comment on column PERFORM_DEFAULT_SCHEDULE.ADMINISTRATION
+  is '给药途径和方法';
+comment on column PERFORM_DEFAULT_SCHEDULE.DEFAULT_SCHEDULE
+  is '缺省的执行时间表';
+-- Create/Recreate indexes
+create unique index IND_1_PERFORM_DEFAULT_SCHEDULE on PERFORM_DEFAULT_SCHEDULE (id);
