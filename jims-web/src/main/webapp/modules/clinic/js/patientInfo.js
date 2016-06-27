597861a057155748cc21dd2fc1695f3a21ae1c5f
zhaoning12 <1174393128@qq.com>
2016-06-27 19:20:38 +0800

    病人基本信息修改

--- jims-web/src/main/webapp/modules/clinic/js/patientInfo.js
+++ jims-web/src/main/webapp/modules/clinic/js/patientInfo.js
@@ -1,5 +1,32 @@
 
 $(function() {
+    /**
+     * 性别下拉框
+     */
+    $('#setId').combobox({
+        data: setData,
+        valueField: 'value',
+        textField: 'label'
+    })
+    $("#setId ").combobox('select',setData[0].value);
+    /**
+     * 身份下拉框
+     */
+    $('#identityId').combobox({
+        data: identityDict,
+        valueField: 'id',
+        textField: 'identityName'
+    })
+    $("#identityId ").combobox('select',identityDict[0].id);
+  /**
+     * 费别下拉框
+     */
+    $('#chargeTypeId').combobox({
+        data: chargeType,
+        valueField: 'id',
+        textField: 'charge_type_name'
+    })
+    $("#chargeTypeId ").combobox('select',chargeType[0].id);
     var clinicId=$("#clinicMasterId",parent.document).val();
     $.ajax({
         'type': 'get',
