0d462d279200305a73e44f8b181a0774620317e9
wei <742041253@qq.com>
2016-06-28 16:54:55 +0800

    修改【药品价格维护】大类+亚类查询

--- jims-service/src/main/java/com/jims/phstock/service/DrugPriceListService.java
+++ jims-service/src/main/java/com/jims/phstock/service/DrugPriceListService.java
@@ -107,15 +107,14 @@ public class DrugPriceListService implements DrugPriceListServiceApi {
 
     /**
      * 根据当前组织结构，和当前类别获取所有的药品名称字典。
-     * @param orgId  组织机构id
      * @param classCode 类别代码
      * @return
      * @author txb
      *
      */
     @Override
-    public List<DrugNameDict> listDrugNameDictByClassCode(String orgId, String classCode) {
-        return bo.listDrugNameDictByClassCode(orgId, classCode);
+    public List<DrugNameDict> listDrugNameDictByClassCode(String classCode) {
+        return bo.listDrugNameDictByClassCode(classCode);
     }
 
     /**
