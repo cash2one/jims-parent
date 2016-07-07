76667e39cd400ab8e3631548e566ac32cb673e8c
zhangyao <83221330@qq.com>
2016-07-07 17:43:05 +0800

    修改入院记录位置

--- jims-service/src/main/java/com/jims/emr/enterHospital/bo/ElectronEnterHospitalBo.java
+++ jims-service/src/main/java/com/jims/emr/enterHospital/bo/ElectronEnterHospitalBo.java
@@ -1,15 +1,13 @@
+package com.jims.emr.enterHospital.bo;
 
-package com.jims.clinic.service;
-
-import com.jims.clinic.api.ElectronEnterHospitalServiceApi;
-import com.jims.clinic.bo.ElectronEnterHospitalBo;
-import com.jims.clinic.dao.ElectronEnterHospitalDao;
-import com.jims.doctor.diagnosis.bo.EmrDiagnosisBo;
-import com.jims.doctor.diagnosis.dao.EmrDiagnosisDao;
-import com.jims.clinic.entity.ElectronEnterHospital;
-import com.jims.diagnosis.entity.EmrDiagnosis;
 import com.jims.common.service.impl.CrudImplService;
+import com.jims.diagnosis.entity.EmrDiagnosis;
+import com.jims.doctor.diagnosis.dao.EmrDiagnosisDao;
+import com.jims.emr.enterHospital.dao.ElectronEnterHospitalDao;
+import com.jims.enterHospital.entity.ElectronEnterHospital;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,36 +17,69 @@ import java.util.List;
  * @author zhaonig
  * @version 2016-04-20
  */
-@com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")
-
-public  class ElectronEnterHospitalServiceImpl  implements ElectronEnterHospitalServiceApi {
+@Service
+@Transactional(readOnly = false)
+public  class ElectronEnterHospitalBo extends CrudImplService<ElectronEnterHospitalDao,ElectronEnterHospital> {
     @Autowired
-	private ElectronEnterHospitalBo electronEnterHospitalBo;
-	@Autowired
-	private EmrDiagnosisBo emrDiagnosisBo;
+    private ElectronEnterHospitalDao electronEnterHospitalDao;
+    @Autowired
+    private EmrDiagnosisDao emrDiagnosisDao;
 
-	/**
-	 * 保存病历文书
-	 * @param electronEnterHospital
-	 * @author pq
-	 * @return
-	 */
-	public String saveEnter(ElectronEnterHospital electronEnterHospital){
-       String num = "";
-		num = electronEnterHospitalBo.saveEnter(electronEnterHospital);
-		return num;
-	}
+    /**
+     * 保存病历文书
+     * @param electronEnterHospital
+     * @author pq
+     * @return
+     */
+    public String saveEnter(ElectronEnterHospital electronEnterHospital){
+        int num = 0;
 
-	/**
-	 * 查询病历文书
-	 * @param electronEnterHospital
-	 * @author pq
-	 * @return
-	 */
-	@Override
-	public ElectronEnterHospital getElectronEnteHos(ElectronEnterHospital electronEnterHospital) {
-		return electronEnterHospitalBo.getElectronEnteHos(electronEnterHospital);
-	}
+        if (electronEnterHospital!=null) {
+            String	str = save(electronEnterHospital);
+            num =Integer.parseInt(str==null?"0":str);
+
+            List<EmrDiagnosis> emrDiagnosisList = electronEnterHospital.getDiagnosisList();
+            if (emrDiagnosisList!=null) {
+
+
+                if (emrDiagnosisList.size() > 0) {
+                    for (int i = 0; i < emrDiagnosisList.size(); i++) {
+                        EmrDiagnosis diagnosis = emrDiagnosisList.get(i);
+                        diagnosis.setDiagnosisParent(electronEnterHospital.getId());
+                        diagnosis.setParentId("0");
+                        try {
+                            if (diagnosis.getIsNewRecord()) {
+                                diagnosis.preInsert();
+                                num = emrDiagnosisDao.insert(diagnosis);
+                            } else {
+                                diagnosis.preUpdate();
+                                num = emrDiagnosisDao.update(diagnosis);
+                            }
+                        } catch (Exception e) {
+                            return num + "";
+                        }
+                        return num + "";
+                    }
+                    return num + "";
+                }
+            }
+        }
+
+
+
+        return num+"";
+    }
+
+    /**
+     * 查询病历文书
+     * @param electronEnterHospital
+     * @author pq
+     * @return
+     */
+
+    public ElectronEnterHospital getElectronEnteHos(ElectronEnterHospital electronEnterHospital) {
+        return electronEnterHospitalDao.getElectronEnteHos(electronEnterHospital);
+    }
 
 
 }
