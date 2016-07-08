2e9752eae39b0a110f7dad8ad37d145576935190
zhaoning12 <1174393128@qq.com>
2016-07-08 17:25:10 +0800

    病案首页

---
+++ jims-api/src/main/java/com/jims/clinic/vo/MedicalRecordVo.java
@@ -0,0 +1,63 @@
+package com.jims.clinic.vo;
+
+import com.jims.clinic.dto.InpBillDetailDto;
+import com.jims.clinic.entity.EmrDiagnosis;
+import com.jims.common.persistence.DataEntity;
+import com.jims.operation.entity.OperationSchedule;
+import com.jims.patient.entity.PatMasterIndex;
+import com.jims.patient.entity.PatVisit;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/7/7.
+ * 病案首页 VO
+ * @author zhaoning
+ */
+public class MedicalRecordVo extends DataEntity<MedicalRecordVo> {
+   private PatVisit patVisit;
+   private PatMasterIndex patMasterIndex;
+   private List<EmrDiagnosis> emrDiagnosises;
+   private List<OperationSchedule> operationSchedules;
+   private InpBillDetailDto inpBillDetailDto;
+
+    public PatVisit getPatVisit() {
+        return patVisit;
+    }
+
+    public void setPatVisit(PatVisit patVisit) {
+        this.patVisit = patVisit;
+    }
+
+    public PatMasterIndex getPatMasterIndex() {
+        return patMasterIndex;
+    }
+
+    public void setPatMasterIndex(PatMasterIndex patMasterIndex) {
+        this.patMasterIndex = patMasterIndex;
+    }
+
+    public List<EmrDiagnosis> getEmrDiagnosises() {
+        return emrDiagnosises;
+    }
+
+    public void setEmrDiagnosises(List<EmrDiagnosis> emrDiagnosises) {
+        this.emrDiagnosises = emrDiagnosises;
+    }
+
+    public List<OperationSchedule> getOperationSchedules() {
+        return operationSchedules;
+    }
+
+    public void setOperationSchedules(List<OperationSchedule> operationSchedules) {
+        this.operationSchedules = operationSchedules;
+    }
+
+    public InpBillDetailDto getInpBillDetailDto() {
+        return inpBillDetailDto;
+    }
+
+    public void setInpBillDetailDto(InpBillDetailDto inpBillDetailDto) {
+        this.inpBillDetailDto = inpBillDetailDto;
+    }
+}
