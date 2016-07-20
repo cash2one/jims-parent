2e964b86a055bc9024fac013df021d44dd39c2c5
pangqian <1525801249@qq.com>
2016-07-20 11:31:13 +0800

    修改出院记录的保存

--- jims-service/src/main/resources/db/migration/V246__alter_electron_leave_hospital.sql
+++ jims-service/src/main/resources/db/migration/V246__alter_electron_leave_hospital.sql
@@ -8,4 +8,4 @@ alter table ELECTRON_LEAVE_HOSPITAL modify VISIT_ID NUMBER(5);
 alter table ELECTRON_LEAVE_HOSPITAL add ORG_ID VARCHAR2(64);
 -- Add comments to the columns 
 comment on column ELECTRON_LEAVE_HOSPITAL.ORG_ID
-  is '��������';
+  is '机构代码';
