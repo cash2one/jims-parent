1d85740ca5dcda934e0e30800cbab873e252651b
zhangyao <83221330@qq.com>
2016-07-26 17:53:31 +0800

    修改项目价表视图

--- jims-service/src/main/resources/db/migration/V221__add_v_clinic_name_price.sql
+++ jims-service/src/main/resources/db/migration/V221__add_v_clinic_name_price.sql
@@ -4,7 +4,7 @@
 /*=============================================================*/
 
 create or replace view v_clinic_name_price as
-select c.item_class,c.item_code,c.item_name,c.item_spec,
+select c.item_class,a.item_code,c.item_name,c.item_spec,
        c.units,nvl(b.amount,1)*nvl(c.price,0) as price,c.prefer_price,c.performed_by,c.subj_code,a.INPUT_CODE,
        b.charge_item_no
 from clinic_item_name_dict a,
