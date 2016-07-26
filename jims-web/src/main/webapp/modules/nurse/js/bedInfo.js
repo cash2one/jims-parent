5422c74b9592faafef7a3438a8550d6d8c6cdb31
pangqian <1525801249@qq.com>
2016-07-26 11:22:19 +0800

    修改床位信息的样式

--- jims-web/src/main/webapp/modules/nurse/js/bedInfo.js
+++ jims-web/src/main/webapp/modules/nurse/js/bedInfo.js
@@ -1,7 +1,5 @@
 var wardCode='160101';
 $(function(){
-
-
     $.ajax({
         method:"POST",
         dataType: 'json',
@@ -41,9 +39,27 @@ $(function(){
                 }
                 if(item.sex!=null){
                      if(item.sex=='1'){//女
-                         html = html+'<div class="cus-sex-img"><img src="/static/images/index/female.png" width="40" height="40"/></div></div>';
+                         if(item.age!=null){
+                             if(0<item.age<14){//儿童
+                                 html = html+'<div class="cus-sex-img"><img src="/static/images/index/girl.png" width="40" height="40"/></div></div>';
+                             }else if(item.age>60){//老年人
+                                 html = html+'<div class="cus-sex-img"><img src="/static/images/index/old-female.png" width="40" height="40"/></div></div>';
+                             }else{
+                                 html = html+'<div class="cus-sex-img"><img src="/static/images/index/female.png" width="40" height="40"/></div></div>';
+                             }
+                         }
+
                      }else if(item.sex=='2'){//男
-                         html = html+'<div class="cus-sex-img"><img src="/static/images/index/male.png" width="40" height="40"/></div></div>';
+                         if(item.age!=null){
+                             if(0<item.age<14){//儿童
+                                 html = html+'<div class="cus-sex-img"><img src="/static/images/index/boy.png" width="40" height="40"/></div></div>';
+                             }else if(item.age>60){//老年人
+                                 html = html+'<div class="cus-sex-img"><img src="/static/images/index/old-male.png" width="40" height="40"/></div></div>';
+                             }else{
+                                 html = html+'<div class="cus-sex-img"><img src="/static/images/index/male.png" width="40" height="40"/></div></div>';
+                             }
+                         }
+
                      }
                 }else{
                     html = html+'<div class="cus-sex-img"></div></div>';
